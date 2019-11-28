e82cf56fddc7601a01279abda84dec4d23a679cb
szeth <geoffreyoliaro@gmail.com>
2019-11-28 09:58:27 +0300

    initial commit

---
+++ filter-taxonomies-widget.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Filter Taxonomies Widget Main File.
+ *
+ * @package IHS Geo Location
+ */
+
+/*
+Plugin Name:  Filter Taxonomies Widget
+Plugin URI:   http://geoffreyoliaro.github.io
+Description:  This plugin filters the taxonomy and displays the result using Ajax request.
+Version:      1.0.0
+Author:       Geoffrey Oliaro
+Author URI:   https://profiles.wordpress.org/gsayed786
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  taxonomy-widget
+Domain Path:  /languages
+*/
+
+/* Include the Custom functions file */
+require 'custom-functions.php';
