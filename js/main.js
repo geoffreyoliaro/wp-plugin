62feeaf8742224c96783c9fbc47b6ed673d6425d
szeth <geoffreyoliaro@gmail.com>
2019-11-28 11:03:15 +0300

    Working on the nounces

--- js/main.js
+++ js/main.js
@@ -12,33 +12,37 @@
 			productColor = document.querySelector( '#product-color' ),
 			ajaxUrl = postdata.ajax_url,
 			nonceValue = postdata.ajax_nonce;
-		console.log( nonceValue );
-		console.log( productName.value );
-		console.log( productCat.value );
-		console.log( productColor.value );
+	
 
-		var request = $.post(
-			ajaxUrl,   // this url till admin-ajax.php  is given by functions.php wp_localoze_script()
-			{
-				action: 'my_ajax_hook',
-				security: nonceValue,
-				product_name: productName.value,
-				product_cat: productCat.value,
-				product_sub_cat: productSubCat.value,
-				product_color: productColor.value
-			},
-			function( status ){
-				console.log( status );  // result {success: true, data: {…}}data: data_recieved_from_js: {action: "my_ajax_hook", name: "Lawrence", profession: "actress"}hello_world: "hello"__proto__: Objectsuccess: true__proto__: Object
-			}
-		);
 
-		request.done( function ( response ) {
-			console.log( 'The server responded: ');
-			console.log( response );
-		} );
 
-	}
-
+			console.log( nonceValue );
+			console.log( productName.value );
+			console.log( productCat.value );
+			console.log( productColor.value );
+	
+			var request = $.post(
+				ajaxUrl,   // this url till admin-ajax.php  is given by functions.php wp_localoze_script()
+				{
+					action: 'my_ajax_hook',
+					security: nonceValue,
+					product_name: productName.value,
+					product_cat: productCat.value,
+					product_sub_cat: productSubCat.value,
+					product_color: productColor.value
+				},
+				function( status ){
+					console.log( status );  // result {success: true, data: {…}}data: data_recieved_from_js: {action: "my_ajax_hook", name: "Lawrence", profession: "actress"}hello_world: "hello"__proto__: Objectsuccess: true__proto__: Object
+				}
+			);
+	
+			request.done( function ( response ) {
+				console.log( 'The server responded: ');
+				console.log( response );
+			} );
+	
+		}
+	
 	$( '#product-category' ).on( 'change', function () {
 		subCatAjaxRequest()
 	} );
